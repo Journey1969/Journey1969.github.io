f2e4dcb199aacc20753e8e08503c11575178970f
Journey1969 <journey1969@163.com>
2017-03-31 19:04:50 +0800

    Site updated: 2017-03-31 19:04:50

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 (function () {
     let header = document.getElementById('header');
 
-    let color_counter = 60,
-        lightening = true;
+    const COLOR_MIN = 120, COLOR_MAX = 220, GAP = 100;
+
+    let red = COLOR_MIN,
+        green = COLOR_MAX,
+        blue = COLOR_MAX,
+        status = 1;
+
+    let light_red = COLOR_MIN,
+        light_green = COLOR_MAX,
+        light_blue = COLOR_MAX;
 
     setInterval(function () {
-        if (lightening) {
-            if (color_counter < 220) {
-                color_counter++;
-            } else {
-                lightening = false;
-            }
-        } else {
-            if (color_counter > 60) {
-                color_counter--;
-            } else {
-                lightening = true;
-            }
+        switch (status) {
+            case 1:
+                if (green >= COLOR_MIN) { green--; light_green = green - GAP;}
+                else { status = 2; }
+                break;
+            case 2:
+                if (red <= COLOR_MAX) { red++; light_red = red - GAP;}
+                else { status = 3; }
+                break;
+            case 3:
+                if (red >= COLOR_MIN) { red--; light_red = red - GAP;}
+                else { status = 4; }
+                break;
+            case 4:
+                if (green <= COLOR_MAX) { green++; light_green = green - GAP;}
+                else { status = 1; }
+                break;
         }
 
-        header.style.background = "-webkit-linear-gradient(left, rgb(40, " + color_counter + ", 210), rgb(40, " + (280 - color_counter) + ", 210))"
+        let color1 = 'rgb(' + light_red + ', ' + light_green + ', ' + light_blue + ')';
+        let color2 = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+
+        header.style.background = "-webkit-linear-gradient(left, " + color1 + " ," + color2 + " )"
     }, 50);
 })();

a634516717efb8c1af53a2eaf2650f41a3f65ac5
Journey1969 <journey1969@163.com>
2017-04-02 01:41:33 +0800

    Site updated: 2017-04-02 01:41:33

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
 (function($) {
     // add code type
-    $('.highlight').each(function () {
+    $('.highlight').each(function() {
         var code_type = $(this).attr("class").split(' ')[1].toUpperCase();
         $(this).attr("data-code-type", code_type);
     })
 
     // add img wrapper and caption
-    $('.post-content').each(function () {
-        $(this).find('img').each(function () {
-            $(this).wrap('<div class="img-wrapper"></div>');
-            $(this).after('<span class="img-caption">' + this.title + '</span>');
+    $('.post-content').each(function() {
+        $(this).find('img').each(function() {
+            var img = $(this);
+            img.attr('class', 'lazy');
+            img.attr('data-original', img.attr('src'));
+            img.wrap('<div class="img-wrapper"></div>');
+            img.after('<span class="img-caption">' + this.title + '</span>');
         });
     })
 
     // cancel min-height of img wrapper after images all loaded
-    $('#main-container').imagesLoaded().done(function () {
-        $('.post-content').each(function () {
-            $(this).find('.img-wrapper').each(function () {
+    $('#main-container').imagesLoaded().done(function() {
+        $('.post-content').each(function() {
+            $(this).find('.img-wrapper').each(function() {
                 $(this).css('min-height', '0')
             })
         })
     })
+
+    // lazyload
+    $(function() {
+        $("img.lazy").lazyload({effect: "fadeIn"});
+    });
 })(jQuery);

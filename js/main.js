8b463f43c688472ebacfd8681d2cadf3bf0bd77e
Journey1969 <journey1969@163.com>
2017-04-03 00:32:41 +0800

    Site updated: 2017-04-03 00:32:39

--- js/main.js
+++ js/main.js
@@ -9,11 +9,20 @@
     $('.post-content').each(function(i) {
         $(this).find('img').each(function() {
             var img = $(this);
-            img.attr('class', 'lazy');
-            img.attr('data-original', img.attr('src'));
+            var origin_img_src = img.attr('src');
+
+            // create compressed images' source path
+            var path = origin_img_src.split('/');
+            path.push("min__" + path.pop());
+            min_img_src = path.join("/");
+            console.log(min_img_src);
+
+            // img.attr('class', 'lazy');
+            // img.attr('data-original', min_img_src);
+            img.attr('src', min_img_src);
             img.wrap('<div class="img-wrapper img-loading"></div>');
             img.after('<p class="img-caption">' + this.alt + '</p>');
-            img.wrap('<a href="' + this.src + '" title="' + this.alt + '" class="fancybox" rel="article"></a>');
+            img.wrap('<a href="' + origin_img_src + '" title="' + this.alt + '" class="fancybox" rel="article"></a>');
         });
     })
 
@@ -28,9 +37,11 @@
     })
 
     // activate lazyload
+    /*
     $(function() {
         $("img.lazy").lazyload();
     });
+    */
 
     // activate fancybox
     $('a.fancybox').fancybox();
